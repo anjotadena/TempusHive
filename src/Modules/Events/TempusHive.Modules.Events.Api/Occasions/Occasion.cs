26f8d4204fd7902b839731ecd333c86a223b5fb5
Anjo Tadena <tadena.anjo@gmail.com>
2024-12-30 20:42:13 +0800

    Add Occasion Status

--- src/Modules/Events/TempusHive.Modules.Events.Api/Occasions/Occasion.cs
+++ src/Modules/Events/TempusHive.Modules.Events.Api/Occasions/Occasion.cs
@@ -2,6 +2,17 @@
 
 public sealed class Occasion
 {
+    public Guid Id { get; set; }
 
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public string Location { get; set; }
+
+    public DateTime StartsAtUtc { get; set; }
+
+    public DateTime? EndsAtUtc { get; set; }
+
+    public OccasionStatus Status { get; set; }
 }
-
---
+++ src/Modules/Events/TempusHive.Modules.Events.Api/Occasions/OccasionStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace TempusHive.Modules.Occasions.Api.Occasions;
+
+public enum OccasionStatus
+{
+    Draft = 0,
+    Published = 1,
+    Completed = 2,
+    Cancelled = 3
+}
